03dabb0469a94167c4ea1ec657029c39561888c6
tamer <tamer@tamer>
2021-11-16 06:44:20 +0100

    move muted & deaf users to bed

--- stfu/index.js
+++ stfu/index.js
@@ -3,6 +3,8 @@ const sql = new SQLite("./muted.sqlite");
 
 const { Client, Intents, Permissions } = require('discord.js');
 const { token } = require('./config.json');
+const bed = "900509228257660998"; // AFK voice channel id
+const la = "900463544754143232"; // LA id
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS] });
 
@@ -19,17 +21,20 @@ client.once('ready', () => {
   	client.delMute = sql.prepare("DELETE FROM muted WHERE member = ? AND guild = ?");
 });
 
-client.on('voiceStateUpdate', (oldState, newState) => {
+client.on('voiceStateUpdate', async (oldState, newState) => {
 	if (!oldState.serverMute && newState.serverMute) {
 		let muted = { member: `${newState.id}`, guild:`${newState.guild.id}` };
 		client.setMute.run(muted);
 	}
-	if (oldState.serverMute && !newState.serverMute) {
+	else if (oldState.serverMute && !newState.serverMute) {
 		client.delMute.run(newState.id, newState.guild.id);
 	}
-	if (!oldState.channelId && newState.channelId &&
+	else if (!oldState.channelId && newState.channelId &&
 		client.checkMute.get(newState.id, newState.guild.id)) {
-			oldState.setMute(true).catch(e => console.error(e));
+			await oldState.setMute(true).catch(e => console.error(e));
+	}
+	else if (newState.guild.id === la && newState.channelId != bed && newState.selfMute && newState.selfDeaf) {
+		await newState.member.voice.setChannel(bed).catch(e => console.log(e));
 	}
 });
 client.login(token);
